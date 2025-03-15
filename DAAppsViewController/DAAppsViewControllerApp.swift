1f16f5ba02b83bf713c69dcdb162d0b1dddbcf2f
Ricardo N <rnaharro@icloud.com>
2025-03-15 21:06:50 +0100

    Initial Commit

---
+++ DAAppsViewController/DAAppsViewControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAAppsViewControllerApp.swift
+//  DAAppsViewController
+//
+//  Created by Ricardo Naharro on 15/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DAAppsViewControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
